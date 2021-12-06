9be707177e363c7186bc9cd5a04a4ea507358c79
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-12-06 17:29:14 +0100

    day 2 first part completed

--- day-02.rb
+++ day-02.rb
@@ -2,6 +2,21 @@ require_relative 'common'
 
 class Day2 < AdventDay
   def first_part
+    x = 0
+    y = 0
+    input.each do |data|
+      data = data.split
+      nb = data.last.to_i
+      case data.first
+      when "forward"
+        x += nb
+      when "up"
+        y -= nb
+      when "down"
+        y += nb
+      end
+    end
+    coordinates = x * y
   end
 
   def second_part
@@ -11,6 +26,10 @@ class Day2 < AdventDay
 
   def convert_data(data)
     super
+  end
+
+  def debug_input
+    "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
   end
 end
 
